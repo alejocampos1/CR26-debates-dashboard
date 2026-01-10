19b544bc0ee59b65e278635f45ed604767eaee2e
Alejandro Campos <alee.1894@gmail.com>
2026-01-10 17:43:41 -0600

    20260110 - Dashboard Update

--- app.py
+++ app.py
@@ -429,9 +429,7 @@ with col_footer_text:
 with col_footer_logo:
     st.markdown(
         """
-        <a href="https://www.facebook.com/crsoundcheck" target="_blank">
             <img src="assets/logo.png" style="max-width:120px; opacity:0.9;">
-        </a>
         """,
         unsafe_allow_html=True,
     )

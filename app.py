2ef830cd796229e62dc81f18aeeefafed3800024
Alejandro Campos <alee.1894@gmail.com>
2026-01-09 20:38:14 -0600

    20260109 - Dashboard Update

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+from streamlit_autorefresh import st_autorefresh
 from config.settings import CADENA_CONEXION_POSTGRES
 from db.connection import obtener_conexion
 from queries.debate_metrics import (

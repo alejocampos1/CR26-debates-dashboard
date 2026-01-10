a8dd6d3ac4b3c6f1f1c2e8072197537edacf958b
Alejandro Campos <alee.1894@gmail.com>
2026-01-09 19:47:05 -0600

    20260109 - Dashboard Update

--- queries/base.py
+++ queries/base.py
@@ -3,16 +3,21 @@ import pandas as pd
 
 def obtener_menciones_base(conexion):
     query = """
-        SELECT
-            debate_id,
-            candidate,
-            platform,
-            content_type,
-            sentiment_label,
-            sentiment_score,
-            original_timestamp
-        FROM ocdul_debates.mentions_raw
-        WHERE is_valid = true
-    """
+            SELECT
+                debate_id,
+                candidate,
+                platform,
+                content_type,
+                sentiment_label,
+                sentiment_score,
+                original_timestamp
+            FROM ocdul_debates.mentions_raw
+            WHERE
+                is_valid = TRUE
+                AND (
+                    original_timestamp AT TIME ZONE 'America/Costa_Rica'
+                ) >= TIMESTAMP '2026-01-09 18:00:00'
+            ;
+            """
 
     return pd.read_sql(query, conexion)

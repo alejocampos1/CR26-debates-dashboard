36f94dea165f95b9d2197d48842e435fc1cb7e5d
Alejandro Campos <alee.1894@gmail.com>
2026-01-11 17:46:13 -0600

    20260111 - Dashboard Update

--- app.py
+++ app.py
@@ -208,7 +208,7 @@ st.caption("### Monitoreo de la conversación digital en tiempo real")
 st.caption("##### Una iniciativa de: [SoundCheck CR](https://www.facebook.com/crsoundcheck)")
 st.markdown("---")
 
-st.markdown("## Debate Presidencial")
+st.markdown("## Debate Presidencial - 11 de Enero 2026")
 st.markdown("### Tribunal Supremo de Elecciones - Día 3")
 st.markdown("---")
 
